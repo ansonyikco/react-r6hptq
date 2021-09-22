c5cbb2ce732ea508caa84e00291170abfa096463
ansonyikco <ansonyikco@gmail.com>
2021-09-22 23:31:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-r6hptq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-r6hptq)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+
+
+  
+  return (
+    <div>
+      <h1>Hello Anson Yik!</h1>
+      <p>Anson Yik is learning :)</p>
+      
+    </div>
+  );
+}
